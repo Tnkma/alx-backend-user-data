5a0392f839863d6b9e8f247dfec1083dbd6ef67a
Godswill <onwusilikegodswill@gmail.com>
2024-06-09 18:01:23 +0100

    fixed the requiremnets

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 import re
 from typing import List
 
+
 def filter_datum(
-    fields: list[str], redaction: str, message: str, separator: str
-    ) -> str:
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """filter_datum that returns the log message obfuscated
 
     Args:
