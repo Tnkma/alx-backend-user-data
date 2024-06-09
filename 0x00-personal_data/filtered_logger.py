a15f3e11267cfc828e3621846ee2b61f9322dc8e
Godswill <onwusilikegodswill@gmail.com>
2024-06-09 18:35:55 +0100

    fixed pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ filtered logger """
 import re
+import logging
 from typing import List
 
 
@@ -17,3 +18,31 @@ def filter_datum(
     """
     pattern = f"({'|'.join(fields)})=([^\\{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method that returns the logRecord
+
+        Args:
+            record (logging.LogRecord): the incoming log record
+
+        Returns:
+            str: returns the filtered value
+        """
+        incoming_message = super(RedactingFormatter, self).format(record)
+        text = filter_datum(
+            self.fields, self.REDACTION, incoming_message, self.SEPARATOR
+        )
+        return text
