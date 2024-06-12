688e7e80862061ad8bc64f61777fb2d333439fac
Godswill <onwusilikegodswill@gmail.com>
2024-06-12 12:19:27 +0100

    :wrench:fixed the quotation issue

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
