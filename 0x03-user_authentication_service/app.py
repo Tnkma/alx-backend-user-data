d35ca91cad15cca244def584bcaf40389be212e7
Godswill <onwusilikegodswill@gmail.com>
2024-06-14 22:13:11 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,21 @@ def login() -> str:
     return response
 
 
+# logout method that implements the DELETE /sessions route.
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ implement the end-point to logout a user
+
+    Returns:
+        str: the successful logout message
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return make_response({"message": "logout successful"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
