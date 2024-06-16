43bd6275204f4b4027292e3e6dedc3e54226138b
Godswill <onwusilikegodswill@gmail.com>
2024-06-16 15:51:54 +0100

    fixing the return response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -125,9 +125,9 @@ def update_password() -> str:
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
