61008ad48a3a846816fffd83a63fbbcaddf50144
Godswill <onwusilikegodswill@gmail.com>
2024-07-28 11:33:22 +0100

    fix pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,12 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
- 
+
 
 @app_views.route('/forbidden', strict_slashes=False)
 def forbidden() -> str:
     """ forbidden handler """
     abort(403)
+
 
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
