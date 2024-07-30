61b3aec19061f173a4b36f547d7c9ae116588811
Godswill <onwusilikegodswill@gmail.com>
2024-07-30 20:16:05 +0100

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,7 +17,7 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
-    
+
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
     ) -> str:
@@ -27,6 +27,8 @@ class BasicAuth(Auth):
         ) is not str:
             return None
         try:
-            return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(
+                base64_authorization_header
+            ).decode('utf-8')
         except Exception:
             return None
