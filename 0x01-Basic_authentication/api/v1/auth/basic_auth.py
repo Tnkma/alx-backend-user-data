56d0a2c1a5688b7bd41601949755366001afb28e
Godswill <onwusilikegodswill@gmail.com>
2024-07-30 20:10:21 +0100

    Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class"""
     pass
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """ Extracts the base64 part of the Authorization header"""
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
