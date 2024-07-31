87d61e0e64a2e4230a6d1998c81448ed733eaa1a
Godswill <onwusilikegodswill@gmail.com>
2024-07-31 14:10:35 +0100

    UserSession

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -28,7 +28,10 @@ class SessionDBAuth(SessionExpAuth):
         return user_sessions[0].user_id
 
     def destroy_session(self, request=None):
-        """Destroy the UserSession based on the Session ID from the request cookie"""
+        """
+        Destroy the UserSession based on the 
+        Session ID from the request cookie
+        """
         if request is None:
             return False
         session_id = self.session_cookie(request)
