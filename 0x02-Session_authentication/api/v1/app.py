b7b308dbde6f3114dcbd1a38dafbcdba295ab6b1
Godswill <onwusilikegodswill@gmail.com>
2024-07-31 11:53:36 +0100

    Retrive current_user

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,6 +61,7 @@ def handle_before_request():
     if request.current_user is None:
         abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
